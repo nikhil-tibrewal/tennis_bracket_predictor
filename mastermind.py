04ff1f42d3627adf8aa2ac716b2b36dc2b3f82a8
Nikhil Tibrewal <nikhilt@spotify.com>
2015-05-19 19:52:31 -0400

    Add player bio data crawler and into the model
    Change-Id: I10163e431ec79c2fec704c3d6da3845ea0da63c5

--- mastermind.py
+++ mastermind.py
@@ -10,6 +10,8 @@ import random
 import scipy.io as sio
 import numpy as np
 import datetime as dt
+import random
+import sys
 
 from sklearn.metrics import accuracy_score
 import sklearn.linear_model
@@ -47,19 +49,32 @@ def sanityCleanPlayer(player):
         player = sanityDict[player]
     return player
 
+def getPlaysValue(plays):
+    if plays == 'Right':
+        return 0
+    return 1
+
 dataFiles = ['2008.csv', '2009.csv', '2010.csv', '2011.csv', '2012.csv', '2013.csv', '2014.csv']
 playerBioFile = 'player_bio.txt'
 
 # Load Player bios in memory
 playerBios = {}
 notFoundBios = set()
+missingDataBios = set()
 with open(playerBioFile, 'rU') as f:
     for line in f:
-        parts = line.split(',')
-        if parts[0] in playerBios:
-            playerBios[parts[0]].append(parts[1:])
+        parts = line.strip().split(',')
+        if parts[2] == ' Republic of':
+            parts[2] = parts[2] + ',' + parts[3]
+            parts.pop(3)
+
+        if len(parts) == 8:
+            if parts[0] in playerBios:
+                playerBios[parts[0]].append(parts[1:])
+            else:
+                playerBios[parts[0]] = [parts[1:]]
         else:
-            playerBios[parts[0]] = [parts[1:]]
+            missingDataBios.add(line)
 
 # Get training data for matches
 matchData_X = []
@@ -70,6 +85,7 @@ numSurfaces = 4
 (courtTypes, courtId) = ({}, 0)
 reqdFields = ['Date', 'Court', 'Surface', 'Best of', 'WRank', 'LRank', 'WPts', 'LPts', 'Winner', 'Loser', 'Comment', 'Lsets'] # Fields needed in training data
 dateFormat = '%m/%d/%y'
+birthDateFormat = '%d.%m.%Y'
 
 for dataFile in dataFiles:
     print 'Scanning file ' + dataFile + '....'
@@ -77,8 +93,6 @@ for dataFile in dataFiles:
         reader = csv.reader(f)
         headers = reader.next()
         fieldsIdxMap = getIndices(headers, reqdFields)
-        # headerIndices, winnerIdx, loserIdx, surfaceIdx, commentIdx, winnerRankIdx, loserRankIdx, courtIdx, dateIdx = getIndices(headers, reqdFields)
-        # headerIndices = np.array(headerIndices)
 
         for row in reader:
             if (row[fieldsIdxMap['Comment']] == 'Completed' and
@@ -120,11 +134,21 @@ for dataFile in dataFiles:
                     courtId += 1
                 court = float(courtTypes[row[fieldsIdxMap['Court']]])
 
+                # Get sets lots in previous match, and days since previous match
                 currentMatchDate = dt.datetime.strptime(row[fieldsIdxMap['Date']], dateFormat)
                 prevWinnerMatchDate, prevLoserMatchDate = (dt.datetime.strptime(prevWinnerMatch[fieldsIdxMap['Date']], dateFormat), dt.datetime.strptime(prevLoserMatch[fieldsIdxMap['Date']], dateFormat))
                 daysPrevMatchWinner, daysPrevMatchLoser = ((currentMatchDate - prevWinnerMatchDate).days, (currentMatchDate - prevLoserMatchDate).days)
                 prevLSetsWinner, prevLSetsLoser = (float(prevWinnerMatch[fieldsIdxMap['Lsets']]) / float(prevWinnerMatch[fieldsIdxMap['Best of']]),
                     float(prevLoserMatch[fieldsIdxMap['Lsets']]) / float(prevLoserMatch[fieldsIdxMap['Best of']]))
+
+                # Get player biodata
+                bioWinner, bioLoser = (random.choice(bioWinner), random.choice(bioLoser))
+                winnerBirthDate, loserBirthDate = (dt.datetime.strptime(bioWinner[2], birthDateFormat), dt.datetime.strptime(bioLoser[2], birthDateFormat))
+                winnerAge, loserAge = ((winnerBirthDate - currentMatchDate).days, (loserBirthDate - currentMatchDate).days)
+                winnerHeight, loserHeight = (float(bioWinner[3]), float(bioLoser[3]))
+                winnerWeight, loserWeight = (float(bioWinner[4]), float(bioLoser[4]))
+                winnerPlays, loserPlays = (getPlaysValue(bioWinner[5]), getPlaysValue(bioLoser[5]))
+                winnerProAge, loserProAge = (currentMatchDate.year - int(bioWinner[6]), currentMatchDate.year - int(bioLoser[6]))
 
                 # Set surfaces
                 surfaces = [0 for i in xrange(numSurfaces)]
@@ -136,17 +160,18 @@ for dataFile in dataFiles:
                     float(r[fieldsIdxMap['WRank']]), float(r[fieldsIdxMap['LRank']]),
                     float(r[fieldsIdxMap['WPts']]), float(r[fieldsIdxMap['LPts']]), float(daysPrevMatchWinner),
                     float(daysPrevMatchLoser), float(surfaces[0]), float(surfaces[1]), float(surfaces[2]), float(surfaces[3]),
-                    prevLSetsWinner, prevLSetsLoser])
+                    prevLSetsWinner, prevLSetsLoser, float(winnerAge), float(loserAge), float(winnerHeight), float(loserHeight),
+                    float(winnerWeight), float(loserWeight), float(winnerPlays), float(loserPlays), float(winnerProAge), float(loserProAge)])
                 y = 0 # Left is the winner
                 if (random.random() >= 0.5):
                     # court, best of,  wRank, lRank, wPts, lPts, days since previous match P1, days previous match P2, 4 surface dummies
-                    r = np.array([r[0], r[1], r[3], r[2], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11], r[12], r[13]])
+                    r = np.array([r[0], r[1], r[3], r[2], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11], r[12], r[13], r[14], r[15], r[16], r[17], r[18], r[19], r[20], r[21], r[22], r[23]])
                     y = 1 # Right is the winner
                 matchData_X.append(r)
                 matchData_Y.append(y)
-                # matchData.append((r, y))
 
-print notFoundBios
+print 'Not found player biodata for:', notFoundBios
+print 'Missing info player data for %d players' % len(missingDataBios)
 
 matchData_X = np.array(matchData_X)
 matchData_Y = np.array(matchData_Y)
@@ -163,7 +188,7 @@ print courtTypes
 # At this point we have all the training data we need
 # Train on first N data points
 
-(train_X, test_X, train_Y, test_Y) = (matchData_X[:16000], matchData_X[16000:], matchData_Y[:16000], matchData_Y[16000:])
+(train_X, test_X, train_Y, test_Y) = (matchData_X[:13000], matchData_X[13000:], matchData_Y[:13000], matchData_Y[13000:])
 model = sklearn.linear_model.LogisticRegression(penalty='l2')
 model = model.fit(train_X, train_Y)
 
